a539121ac0dafc71b704360cc1cb581cfdc52853
ZachDaChampion <zachchampion79@gmail.com>
2018-12-16 16:43:52 -0500

    added position tracking

---
+++ include/lib/position_tracker.hpp
@@ -0,0 +1,41 @@
+#ifndef POSITION_TRACKER_H_
+#define POSITION_TRACKER_H_
+
+#include "main.h"
+#define USE_USE_MATH_DEFINES
+#include <math.h>
+
+namespace position_tracker {
+
+  // chassis characteristics
+  #define TRACKER_DIST 15.25f // distance between left/right tracker wheels (inches)
+  #define SIDE_DIST 7.5f // distance from center of rotation to sideways tracker wheel (inches)
+  #define WHEEL_SIZE 4.125f // diameter of tracking wheels (inches)
+
+  // conversion factors
+  #define ANGLE_TO_DIST(angle) WHEEL_SIZE * M_PI * angle/360.f // convert angle (degrees) to dist (inches)
+  #define DIST_TO_ANGLE(dist) dist * 360.f / (WHEEL_SIZE * M_PI) // convert dist (inches) to angle (degrees)
+  #define ORIENTATION_FROM_SIDE_DIST(dist_l, dist_r) (dist_r - dist_l) / TRACKER_DIST // calculate absolute orientation (radians) from left/right distances (inches)
+
+  // sensors
+  extern pros::ADIEncoder* enc_left; // encoder attached to left tracker wheel
+  extern pros::ADIEncoder* enc_right; // encoder attached to right tracker wheel
+  extern pros::ADIEncoder* enc_side; // encoder attached to sideways tracker wheel
+
+  // state
+  extern float x; // position along x axis (inches)
+  extern float x_vel; // velocity along x axis (inches/sec)
+  extern float y; // position along y axis (inches)
+  extern float y_vel; // velocity along y axis (inches/sec)
+  extern long double orientation; // orientation (radians)
+  extern long double angular_vel; // angular velocity (deg/sec)
+
+  // initialize
+  void init(float _x, float _y, float _orientation);
+
+  // update
+  void update(int delta_t);
+
+}
+
+#endif
---
+++ src/lib/position_tracker.cpp
@@ -0,0 +1,101 @@
+#include "lib/position_tracker.hpp"
+
+namespace position_tracker {
+
+
+  // sensors
+  pros::ADIEncoder* enc_left;
+  pros::ADIEncoder* enc_right;
+  pros::ADIEncoder* enc_side;
+
+
+  // known values
+  float left_dist;
+  float right_dist;
+  float side_dist;
+
+
+  // state
+  float x = 0;
+  float x_vel = 0;
+  float y = 0;
+  float y_vel = 0;
+  long double orientation = 0;
+  long double angular_vel = 0;
+
+
+  // initialize
+  void init(float _x, float _y, float _orientation) {
+
+    // init variables
+    x = _x;
+    y = _y;
+    orientation = _orientation;
+
+    // init encoders
+    while (pros::millis() < 200) pros::delay(10); // ADI is unstable when program is first started
+    pros::ADIEncoder enc_left_initializer('A', 'B', false); enc_left = &enc_left_initializer;
+    pros::ADIEncoder enc_right_initializer('C', 'D', false); enc_right = &enc_right_initializer;
+    pros::ADIEncoder enc_side_initializer('E', 'F', false); enc_side = &enc_side_initializer;
+  }
+
+
+  // update
+  void update(int delta_t) {
+
+    // calculate known values
+    float dist = (ANGLE_TO_DIST((enc_left->get_value() + enc_right->get_value()) * .5f)); // distance travelled as measured by left/right encoders
+    float dist_side_wheel = ANGLE_TO_DIST(enc_side->get_value()); // distance travelled as measured by side encoder
+    long double prev_orientation = orientation;
+    orientation = ORIENTATION_FROM_SIDE_DIST(enc_right->get_value(), enc_left->get_value()); // absolute robot orientation (radians)
+    long double delta_orientation = orientation - prev_orientation; // change in orientation since last update
+
+    // define for later
+    float absolute_delta_x = 0;
+    float absolute_delta_y = 0;
+
+    // if no change in angle, calculate linearly
+    if(prev_orientation == orientation) {
+      absolute_delta_x = dist * cos(orientation) + dist_side_wheel * cos(orientation + M_PI/2);
+      absolute_delta_y = dist * sin(orientation) + dist_side_wheel * sin(orientation + M_PI/2);
+    }
+
+    // otherwise, calculate via arc-based algorithm
+    else {
+
+      // calculate values
+      long double reference_angle = fabs(delta_orientation); // abs of delta_orientation
+      float dist_side = dist_side_wheel - (SIDE_DIST * delta_orientation); // net dist of sideways wheel after that which is required for robot rotation
+      float radius = fabs(dist / delta_orientation); // radius of left/right arc
+      float radius_side = fabs(dist_side / delta_orientation); // radius of sideways arc
+
+      // calculate reference delta positions
+      float delta_x = cos(reference_angle - (M_PI * .5)) * radius;
+      float delta_y = sin(reference_angle - (M_PI * .5)) * radius + radius;
+      float delta_x_side = -cos(reference_angle) * radius_side + radius_side;
+      float delta_y_side = sin(reference_angle) * radius_side;
+
+      // mirror over applicable axes
+      delta_x *= (dist < 0) ? -1 : 1;
+      delta_y *= (dist * delta_orientation < 0) ? -1 : 1;
+      delta_x_side *= (dist_side * delta_orientation > 0) ? -1 : 1;
+      delta_y_side *= (dist_side < 0) ? -1 : 1;
+
+      // combine sideways movement into standard
+      delta_x += delta_x_side;
+      delta_y += delta_y_side;
+
+      // rotate relative to field (rather than bot)
+      absolute_delta_x = (delta_x * cos(prev_orientation)) - (delta_y * sin(prev_orientation));
+      absolute_delta_y = (delta_x * sin(prev_orientation)) + (delta_y * cos(prev_orientation));
+    }
+
+    // calculate new state
+    x += absolute_delta_x;
+    x_vel = (x_vel * .5f) + (absolute_delta_x * (1000.f/delta_t) * .5f);
+    y += absolute_delta_y;
+    y_vel = (y_vel * .5f) + (absolute_delta_y * (1000.f/delta_t) * .5f);
+    // (orientation calculated already)
+    angular_vel = (angular_vel * .5f) + (delta_orientation * (180.f/M_PI) * (1000.f/delta_t) * .5f);
+  }
+}
