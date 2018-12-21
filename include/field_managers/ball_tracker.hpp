7c487eaa17187019493c71c3fe3f5947988aa957
ZachDaChampion <zachchampion79@gmail.com>
2018-12-21 16:07:25 -0500

    added ball tracker

---
+++ include/field_managers/ball_tracker.hpp
@@ -0,0 +1,46 @@
+#ifndef BALL_TRACKER_H_
+#define BALL_TRACKER_H_
+
+#include "../main.h"
+
+namespace ball_tracker {
+
+  // config
+  #define MAX_BAllS 5 // maximum amount of balls to look for
+  #define SENSOR_ANGLE -60 // angle of vision sensor (degrees)
+  #define SENSOR_HEIGHT 12 // height of vision sensor from ground (inches)
+  #define DISTANCE_SCALAR 1800 // scalar used to calculate distance to ball
+
+  // signatures
+  #define BALL_SIG 1
+
+  // stores all data of a ball
+  typedef struct Ball {
+    bool identified;
+    float vision_x;
+    float vision_y;
+    float robot_x;
+    float robot_dist;
+    float width;
+    float height;
+  } Ball;
+
+  // vision sensor
+  extern pros::Vision vision_sensor;
+
+  // array of balls
+  extern int ball_count; // how many balls are currently visible
+  extern Ball balls[MAX_BAllS]; // array of balls currently visible
+
+  // calculate distance from vision y coordinate
+  float calc_ball_dist(float center_y);
+
+  // create ball object
+  Ball create_ball(float vision_x, float vision_y, float width, float height);
+
+  // upate ball list
+  void update();
+
+}
+
+#endif
