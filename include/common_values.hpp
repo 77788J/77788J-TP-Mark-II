43c1b2a8ada8ddf9f37c18e920acfd6796eec172
ZachDaChampion <zachchampion79@gmail.com>
2018-12-21 14:48:30 -0500

    added some resolutions

--- include/common_values.hpp
+++ include/common_values.hpp
@@ -1,6 +1,16 @@
 #ifndef COMMON_VALUES_H_
 #define COMMON_VALUES_H_
 
+// brain screen
+#define BRAIN_SCREEN_WIDTH 480
+#define BRAIN_SCREEN_HIGHT 240
+
+// vision sensor image
+#define VISION_IMAGE_WIDTH 640
+#define VISION_IMAGE_HEIGHT 400
+
+// mathematical constants
 #define PI 3.141592653589793238462643383279502884L
+
 
 #endif
