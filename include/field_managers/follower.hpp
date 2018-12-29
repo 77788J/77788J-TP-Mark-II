ceb60f14810bf88c93443fbd9e4c9445431a968e
ZachDaChampion <zachchampion79@gmail.com>
2018-12-29 17:12:35 -0500

    added placeholder following values

--- include/field_managers/follower.hpp
+++ include/field_managers/follower.hpp
@@ -3,6 +3,19 @@
 
 namespace follower {
 
+  // ball following value
+  static const float follow_dist_ball = 6;
+
+  // cap following values
+  static const float follow_dist_cap = 18;
+  static const float flip_dist_cap = 12;
+  static const float grab_dist_cap = 6;
+
+  // flag following values
+  static const float fire_dist_double = 36;
+  static const float fire_dist_single_far = 144;
+  static const float fire_dist_single_near = 18;
+
   // follow tuning parameters
   typedef struct FollowParams {
     float dist_mult; // multiplier used to determine voltage based on distance
